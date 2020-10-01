b0211347ef0890a24cb3a9d2494314b780aee9d8
AntonioVRI <avidaurri4@gmail.com>
2020-10-01 14:48:46 -0500

    intro-to-testing exercise

--- code.js
+++ code.js
@@ -7,6 +7,14 @@ const helloWorld = function(){
     return "Hello, World!";
 };
 
-function sayHello(){
+// function sayHello(){
+//     return "Hello, Jane!";
+// }
 
+function sayHello(input){
+    if(input === "Alex"){
+        return "Hello, Alex!";
+    }else{
+        return "Hello, Jane!";
+    }
 }
